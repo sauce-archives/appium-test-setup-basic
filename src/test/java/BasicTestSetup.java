1eb0b7597634efae4ed91ea522dcb22b60ff1c94
Giovanni Rago <grago@testobject.com>
2016-02-15 17:32:54 +0100

    ...

--- src/test/java/BasicTestSetup.java
+++ src/test/java/BasicTestSetup.java
@@ -2,6 +2,7 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -10,6 +11,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.util.Random;
 
 public class BasicTestSetup {
 
@@ -24,13 +26,9 @@ public class BasicTestSetup {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
         /* These are the capabilities we must provide to run our test on TestObject. */
-        capabilities.setCapability("testobject_api_key", System.getenv("TESTOBJECT_API_KEY")); // API key through env variable
-        //capabilities.setCapability("testobject_api_key", "YOUR_API_KEY")); // API key hardcoded
-
+        capabilities.setCapability("testobject_api_key", "60286D0A4E704A2FB6C697C52BF437AC");
         capabilities.setCapability("testobject_app_id", "1");
-
-        capabilities.setCapability("testobject_device", System.getenv("TESTOBJECT_DEVICE_ID")); // device id through env variable
-        //capabilities.setCapability("testobject_device", "Motorola_Moto_E_2nd_gen_real"); // device id hardcoded
+        capabilities.setCapability("testobject_device", "Motorola_Moto_G_2nd_gen_real");
         
         /* The driver will take care of establishing the connection, so we must provide
         * it with the correct endpoint and the requested capabilities. */
@@ -49,55 +47,51 @@ public class BasicTestSetup {
 
     /* A simple addition, it expects the correct result to appear in the result field. */
     @Test
-    public void twoPlusTwoOperation() {
+    public void AppFilterTest() {
 
-        /* Get the elements. */
-        MobileElement buttonTwo = (MobileElement)(driver.findElement(By.id("net.ludeke.calculator:id/digit2")));
-        MobileElement buttonPlus = (MobileElement)(driver.findElement(By.id("net.ludeke.calculator:id/plus")));
-        MobileElement buttonEquals = (MobileElement)(driver.findElement(By.id("net.ludeke.calculator:id/equal")));
-        MobileElement resultField = (MobileElement)(driver.findElement(By.xpath("//android.widget.EditText[1]")));
+        new WebDriverWait(driver, 60)
+                .until(ExpectedConditions.presenceOfElementLocated(By.name("Mein Bauprojekt")));
 
-        /* Add two and two. */
-        buttonTwo.click();
-        buttonPlus.click();
-        buttonTwo.click();
-        buttonEquals.click();
+        driver.navigate().back();
 
-        /* Check if within given time the correct result appears in the designated field. */
-        (new WebDriverWait(driver, 30)).until(ExpectedConditions.textToBePresentInElement(resultField, EXPECTED_RESULT_FOUR));
+        new WebDriverWait(driver, 60)
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("com.bosch.tt.de.mybuildingproject:id/editBudget"))).sendKeys("1234");
 
-    }
+        new WebDriverWait(driver, 60)
+                .until(ExpectedConditions.presenceOfElementLocated(By.name("weiter"))).click();
 
-    /* An invalid operation, it navigates to the advanced panel, selects factorial, then minus,
-     * then the equal button. The expected result is an error message in the result field. */
-    @Test
-    public void factorialMinusOperation() {
-
-        /* In the main panel... */
-        MobileElement menuButton = (MobileElement)(driver.findElement(By.id("net.ludeke.calculator:id/overflow_menu")));
-        menuButton.click();
-
-        MobileElement advancedPanelButton = (MobileElement)(new WebDriverWait(driver, 60))
-                .until(ExpectedConditions.presenceOfElementLocated(By.name("Advanced panel")));
-        advancedPanelButton.click();
-
-        /* In the advanced panel... */
-        MobileElement factorialButton = (MobileElement)(new WebDriverWait(driver, 60))
-                .until(ExpectedConditions.presenceOfElementLocated(By.id("net.ludeke.calculator:id/factorial")));
-        factorialButton.click();
-
-        /* In the main panel again. */
-        MobileElement minusButton = (MobileElement)(new WebDriverWait(driver, 60))
-                .until(ExpectedConditions.presenceOfElementLocated(By.id("net.ludeke.calculator:id/minus")));
-        minusButton.click();
-
-        MobileElement equalsButton = (MobileElement)(driver.findElement(By.id("net.ludeke.calculator:id/equal")));
-        equalsButton.click();
-
-        MobileElement resultField = (MobileElement)(driver.findElement(By.xpath("//android.widget.EditText[1]")));
-
-        (new WebDriverWait(driver, 30)).until(ExpectedConditions.textToBePresentInElement(resultField, EXPECTED_RESULT_ERROR));
+        new WebDriverWait(driver, 60)
+                .until(ExpectedConditions.presenceOfElementLocated(By.name("Gewerke anlegen")));
 
     }
+
+//    @Test
+//    public void factorialMinusOperation() {
+//
+//        MobileElement menuButton = (MobileElement)(driver.findElement(By.id("net.ludeke.calculator:id/overflow_menu")));
+//        menuButton.click();
+//
+//        MobileElement advancedPanelButton = (MobileElement)(new WebDriverWait(driver, 60))
+//                .until(ExpectedConditions.presenceOfElementLocated(By.name("Advanced panel")));
+//        advancedPanelButton.click();
+//
+//        /* In the advanced panel... */
+//        MobileElement factorialButton = (MobileElement)(new WebDriverWait(driver, 60))
+//                .until(ExpectedConditions.presenceOfElementLocated(By.id("net.ludeke.calculator:id/factorial")));
+//        factorialButton.click();
+//
+//        /* In the main panel again. */
+//        MobileElement minusButton = (MobileElement)(new WebDriverWait(driver, 60))
+//                .until(ExpectedConditions.presenceOfElementLocated(By.id("net.ludeke.calculator:id/minus")));
+//        minusButton.click();
+//
+//        MobileElement equalsButton = (MobileElement)(driver.findElement(By.id("net.ludeke.calculator:id/equal")));
+//        equalsButton.click();
+//
+//        MobileElement resultField = (MobileElement)(driver.findElement(By.xpath("//android.widget.EditText[1]")));
+//
+//        (new WebDriverWait(driver, 30)).until(ExpectedConditions.textToBePresentInElement(resultField, EXPECTED_RESULT_ERROR));
+//
+//    }
 
 }
